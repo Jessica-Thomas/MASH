d0c402e416fe28619a5505323ff69d72459d35e1
Jessica-Thomas <jessica.thomas648@gmail.com>
2022-03-14 23:06:28 -0400

    updated model. need to figure out comp errors

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,30 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace MASH.Models;
 
 
 public class PageModel
 {
+    public class Spouse 
+    { 
+        public string GoldSpouse { get; set; }
+        public string SilverSpouse { get; set; }
+        public string BronzeSpouse { get; set; }
+        public string UserSpouse { get; set; }
+    }
 
-    public List<SelectListItem> SilverSpouse = new List<SelectListItem>
+    public List<SelectListItem> SilverSpouse = new()
     {
         new SelectListItem { Value = "1", Text = "Meredith Palmer" },
         new SelectListItem { Value = "2", Text = "Dwight Schrute" },
         new SelectListItem { Value = "3", Text = "Jan Levinson" },
         new SelectListItem { Value = "4", Text = "Kevin Mallone" }
     };
-   }
+
+
+    public List<SelectListItem> GoldSpouse = new()
+    {
+        new SelectListItem { Value = "1", Text = "Derek Morgan" },
+        new SelectListItem { Value = "2", Text = "Spencer Reid" },
+        new SelectListItem { Value = "3", Text = "Penelope Garcia" },
+        new SelectListItem { Value = "4", Text = "Emily Prentiss" }
+    };
+
+    public List<SelectListItem> BronzeSpouse = new()
+    {
+        new SelectListItem { Value = "1", Text = "Cersei Lannister" },
+        new SelectListItem { Value = "2", Text = "Gustavo Fring" },
+        new SelectListItem { Value = "3", Text = "Hannibal Lector" },
+        new SelectListItem { Value = "4", Text = "Toby Flenderson" }
+    };
+
+        ViewBag.GoldSpouse = GoldSpouse;
+        ViewBag.SilverSpouse = SilverSpouse;
+        ViewBag.BronzeSpouse = BronzeSpouse;
+}
        
 
     
 
-//public class Spouse
-//{
-//    public string GoldSpouse { get; set; }
-//    public string SilverSpouse { get; set; }
-//    public string BronzeSpouse { get; set; }
-//    public string UserSpouse { get; set; }
-//}
+
 
 //public class Transportation
 //{
