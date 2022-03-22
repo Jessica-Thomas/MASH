b0ecaa73f26edf4dffdbcf778a9cefa442a64b42
Jessica-Thomas <jessica.thomas648@gmail.com>
2022-03-22 18:42:42 -0400

    pushing to work from mac

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MASH.Models;
 
 namespace MASH.Controllers;
@@ -9,7 +8,7 @@ public class HomeController : Controller
 
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, object petList)
     {
         _logger = logger;
     }
@@ -21,6 +20,45 @@ public class HomeController : Controller
         return View(PageModel);
     }
 
+
+    [HttpPost]
+    public IActionResult Submit(PageModel page)
+    {
+        var spouseList = new List<string>();
+        {
+            string? silverChoice = page.SilverSpouse;
+            string? goldChoice = page.GoldSpouse;
+            string? bronzeChoice = page.BronzeSpouse;
+            string? userChoice = page.UserSpouse;
+        };
+
+        var jobList = new List<string>();
+        {
+            string? silverChoice = page.SilverJob;
+            string? goldChoice = page.GoldJob;
+            string? bronzeChoice = page.BronzeJob;
+            string? userChoice = page.UserJob;
+        };
+
+        var transpoList = new List<string>();
+        {
+            string? silverChoice = page.SilverTranspo;
+            string? goldChoice = page.GoldTranspo;
+            string? bronzeChoice = page.BronzeTranspo;
+            string? userChoice = page.UserTranspo;
+        };
+
+        var petList = new List<string>();
+        {
+            string? silverChoice = page.SilverPet;
+            string? goldChoice = page.GoldPet;
+            string? bronzeChoice = page.BronzePet;
+            string? userChoice = page.UserPet;
+        };
+
+        return View(page);
+
+    }
 
 
 }
